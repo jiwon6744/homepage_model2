90c774d77173d82a4d5da8ef5e32e216dfa3e8b4
kimjiwon <skywldnjs21@naver.com>
2017-01-25 10:42:57 +0900

    commit

--- mvc_blog/src/action/member/ReadAction.java
+++ mvc_blog/src/action/member/ReadAction.java
@@ -14,10 +14,11 @@ public class ReadAction implements Action {
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
+		//관리자가 list에서 id클릭하고 사용자 상세정보보기
 		String id = request.getParameter("id");
+		//일반사용자가 로그인 후 메뉴 목록에서 나의 정보 메뉴를 클릭하고 본인 정보 확인.
 		String grade = (String)session.getAttribute("grade");
-		
-		if (id == null) {
+		if (id == null) {//관리자가 아닌 때
 			id = (String)session.getAttribute("id");
 		}
 		MemberDAO dao = new MemberDAO();
